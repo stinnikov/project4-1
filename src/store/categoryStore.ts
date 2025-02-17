00486be1bbccba1c07e6fe0f77b98fd2affcaa0d
shamil tinnikov <shamiltinnikov@gmail.com>
2025-02-17 20:51:15 +0700

    moved the data fetching logic from the server to src/screens/CatalogScreen from useEffect, useState to src/store/categoriesStore

--- src/store/categoryStore.ts
+++ src/store/categoryStore.ts
@@ -1,14 +1,16 @@
 import { create } from 'zustand';
 import { Category } from '@/src/interfaces/Category';
 import { Product } from '@/src/interfaces/Product';
-import { getCategoriesById, getCategoryById } from '@/src/services/CategoryService';
+import { getCategoriesById, getCategoriesDepthZero, getCategoryById } from '@/src/services/CategoryService';
 import { getProductsByCategoryIdAsync } from '@/src/services/ProductService';
 
 interface CategoryStore {
     currentCategory: Category | undefined;
     categories: Category[] | undefined;
     products: Product[] | undefined;
+    loading: boolean,
     fetchCategoryData: (categoryId: string) => Promise<void>;
+    fetchDepthZeroCategories: () => Promise<void>;
 }
 
 export const useCategoryStore = create<CategoryStore>((set) => ({
@@ -18,6 +20,7 @@ export const useCategoryStore = create<CategoryStore>((set) => ({
     loading: true,
     fetchCategoryData: async (categoryId: string) => {
         try {
+            set({ loading: true });
             const [currentCatResponse, currentCatSubCatsResponse] = await Promise.all([
                 getCategoryById(categoryId),
                 getCategoriesById(categoryId),
@@ -31,6 +34,20 @@ export const useCategoryStore = create<CategoryStore>((set) => ({
             }
         } catch (error) {
             console.error(error);
+        }
+        finally {
+            set({ loading: false });
+        }
+    },
+    fetchDepthZeroCategories: async () => {
+        set({ loading: true });
+        try {
+            const response = await getCategoriesDepthZero();
+            set({ categories: response });
+        } catch (error) {
+            console.error(error);
+        } finally {
+            set({ loading: false });
         }
     },
 }));
